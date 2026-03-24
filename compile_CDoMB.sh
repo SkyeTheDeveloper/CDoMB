43d3466f244a70bfd698fa3d65d635ce97a1639e
Skylar Koningin <greekie.via.linux@gmail.com>
2026-03-24 15:50:29 -0400

    updated compile_CDoMB.sh to move the dynamic library to ~/.local/lib or LocalAppData

--- compile_CDoMB.sh
+++ compile_CDoMB.sh
@@ -16,19 +16,21 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     DYLIB_EXT="dylib"
     OBJ_EXT="o"
     LDFLAGS="-Wl,-install_name,@rpath/libCDoMB.dylib"
+    INST_LOC="~/.local/lib"
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     LIB_EXT="lib"
     DYLIB_EXT="dll"
     OBJ_EXT="obj"
     LDFLAGS=""
+    INST_LOC="$LOCALAPPDATA"
 else
     LIB_EXT="a"
     DYLIB_EXT="so"
     OBJ_EXT="o"
     LDFLAGS="-Wl,-soname,libCDoMB.so"
+    INST_LOC="~/.local/lib"
 fi
 
-echo "Compiling objects..."
 for f in *.cpp; do
     if [[ "$f" != "main.cpp" ]]; then
         clang++ @compile_flags.txt -fPIC -c "$f" -o "${f%.cpp}.$OBJ_EXT"
@@ -37,4 +39,13 @@ done
 
 ar rcs "bin/libCDoMB.${LIB_EXT}" *."$OBJ_EXT"
 clang++ -shared $LDFLAGS -o "bin/libCDoMB.${DYLIB_EXT}" *."$OBJ_EXT"
+mkdir -p "$INST_LOC"
+if [[ $SHELL == "/bin/zsh" ]]; then
+    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/$INST_LOC" >> ~/.zshrc
+elif [[ $SHELL == "/bin/bash" ]]; then
+    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/$INST_LOC" >> ~/.bashrc
+else
+    echo "Non-BASH/Non-ZSH shell as default. Please add 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib' to your shell's path."
+fi
+mv -f "bin/libCDoMB.${DYLIB_EXT}" ~/.local/lib
 rm *."$OBJ_EXT"
