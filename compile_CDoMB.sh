31c20893a63d2a8f0d4816b22fe8968f23271046
Skylar Koningin <greekie.via.linux@gmail.com>
2026-03-24 19:05:14 -0400

    Update compile_CDoMB.sh

--- compile_CDoMB.sh
+++ compile_CDoMB.sh
@@ -15,8 +15,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     LIB_EXT="a"
     DYLIB_EXT="dylib"
     OBJ_EXT="o"
-    LDFLAGS="-Wl,-install_name,@rpath/libCDoMB.dylib"
-    INST_LOC="~/.local/lib"
+    LDFLAGS="-fPIC -Wl,-install_name,@rpath/libCDoMB.dylib"
+    INST_LOC="$HOME/.local/lib"
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     LIB_EXT="lib"
     DYLIB_EXT="dll"
@@ -27,25 +27,25 @@ else
     LIB_EXT="a"
     DYLIB_EXT="so"
     OBJ_EXT="o"
-    LDFLAGS="-Wl,-soname,libCDoMB.so"
-    INST_LOC="~/.local/lib"
+    LDFLAGS="-fPIC -Wl,-soname,libCDoMB.so"
+    INST_LOC="$HOME/.local/lib"
 fi
 
 for f in *.cpp; do
     if [[ "$f" != "main.cpp" ]]; then
-        clang++ @compile_flags.txt -fPIC -c "$f" -o "${f%.cpp}.$OBJ_EXT"
+        clang++ @compile_flags.txt -c "$f" -o "${f%.cpp}.$OBJ_EXT"
     fi
 done
 
 ar rcs "bin/libCDoMB.${LIB_EXT}" *."$OBJ_EXT"
 clang++ -shared $LDFLAGS -o "bin/libCDoMB.${DYLIB_EXT}" *."$OBJ_EXT"
 mkdir -p "$INST_LOC"
-if [[ $SHELL == "/bin/zsh" ]]; then
-    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/$INST_LOC" >> ~/.zshrc
-elif [[ $SHELL == "/bin/bash" ]]; then
-    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/$INST_LOC" >> ~/.bashrc
+if [[ $SHELL == "/bin/zsh" || $SHELL == "/usr/bin/zsh" ]]; then
+    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INST_LOC" >> ~/.zshrc
+elif [[ $SHELL == "/bin/bash" || $SHELL == "/usr/bin/bash" ]]; then
+    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INST_LOC" >> ~/.bashrc
 else
-    echo "Non-BASH/Non-ZSH shell as default. Please add 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib' to your shell's path."
+    echo "Non-BASH/Non-ZSH shell as default. Please add 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INST_LOC' to your shell's path."
 fi
-mv -f "bin/libCDoMB.${DYLIB_EXT}" ~/.local/lib
+mv -f "bin/libCDoMB.${DYLIB_EXT}" "$INST_LOC"
 rm *."$OBJ_EXT"
